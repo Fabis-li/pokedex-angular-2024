dac8840eb7103992e93a912afa60ce288b77809f
Fabis-li <fabiano_slima@hotmail.com>
2024-09-25 21:56:53 -0300

    populando lista

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { NavbarComponent } from "./component/navbar/navbar.component";
+import { PokeApiService } from './services/poke-api.service';
+import { Pokemon } from './models/pokemon';
+import { NgForOf } from '@angular/common';
+import { converterParaTitleCase } from './utils/converter-para-title-case';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [NavbarComponent],
+  imports: [NavbarComponent, NgForOf],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
-  title = 'pokedex-angular-2024';
+export class AppComponent implements OnInit {
+
+  public pokemons: Pokemon[];
+
+  constructor(private pokeApiService: PokeApiService){
+    this.pokemons = [];
+  }
+  
+
+  ngOnInit(): void {
+    this.pokeApiService.selecionarTodos().subscribe((res) => {
+      const arrayResultados = res.results as any [];
+      
+      this.pokemons =  arrayResultados.map(obj => {
+        return {
+          nome: converterParaTitleCase(obj.name),
+        }
+      })
+        
+    });
+  }  
 }
+
